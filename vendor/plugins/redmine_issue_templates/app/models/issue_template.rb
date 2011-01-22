36cbadb4b7b966a13751ff8a873057050580ba28
TrinityT <trinityt.ttt@gmail.com>
2011-01-22 16:05:04 +0900

    test

--- vendor/plugins/redmine_issue_templates/app/models/issue_template.rb
+++ vendor/plugins/redmine_issue_templates/app/models/issue_template.rb
@@ -1,4 +1,7 @@
 class IssueTemplate < ActiveRecord::Base
   unloadable
   belongs_to :project
+
+  validates_presence_of :project_id, :title, :description
+  validates_uniqueness_of :title, :scope => :project_id
 end
