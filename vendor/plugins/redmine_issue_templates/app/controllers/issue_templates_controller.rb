ecbbcb3752108646c2790a8082cc659c901c4882
TrinityT <trinityt.ttt@gmail.com>
2011-01-22 16:38:26 +0900

    一覧表示追加

--- vendor/plugins/redmine_issue_templates/app/controllers/issue_templates_controller.rb
+++ vendor/plugins/redmine_issue_templates/app/controllers/issue_templates_controller.rb
@@ -3,18 +3,18 @@ class IssueTemplatesController < ApplicationController
   before_filter :find_project
 
   def index
+    @issue_templates = IssueTemplate.find(:all, :conditions => ["project_id = ?", @project.id])
   end
 
   def create
     @issue_template = IssueTemplate.new    
-    @issue_template.project = @project
-    
+    @issue_template.project_id = @project.id
     # create new template
     if request.post?
       @issue_template.attributes = params[:issue_template]
       if @issue_template.save
         flash[:notice] = l(:notice_successful_create)
-        redirect_to :action => "show", :id => @issue_template.id
+        redirect_to :action => "show", :id => @project,  :template_id => @issue_template.id
       end
     end
   end
@@ -26,6 +26,7 @@ class IssueTemplatesController < ApplicationController
   end
 
   def show
+    @issue_template = IssueTemplate.find(params[:template_id])
   end
 
   def load
