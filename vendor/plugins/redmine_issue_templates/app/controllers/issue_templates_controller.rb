0fcc98026bd9b07189e3e126b58c795961e5c035
TrinityT <trinityt.ttt@gmail.com>
2011-01-22 15:20:43 +0900

    test

---
+++ vendor/plugins/redmine_issue_templates/app/controllers/issue_templates_controller.rb
@@ -0,0 +1,22 @@
+class IssueTemplatesController < ApplicationController
+  unloadable
+  before_filter :find_project
+
+  def index
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+
+  def update
+  end
+
+  def show
+  end
+
+  def load
+  end
+end
--- vendor/plugins/redmine_issue_templates/init.rb
+++ vendor/plugins/redmine_issue_templates/init.rb
@@ -8,4 +8,13 @@ Redmine::Plugin.register :redmine_issue_templates do
   url 'http://example.com/path/to/plugin'
   author_url 'http://example.com/about'
   requires_redmine :version_or_higher => '1.1.0'
+
+  # 権限表示
+  project_module :issue_templates do
+    permission :edit_issue_templates, {:issue_templates => [:create, :update, :destroy]}
+    permission :show_issue_templates, {:issue_templates => [:index, :show, :load]}
+  end
+
+  menu :project_menu, :issue_templates, { :controller => 'issue_templates', :action => 'index' }, :caption => 'テンプレ', :after => 'issues'
+
 end
